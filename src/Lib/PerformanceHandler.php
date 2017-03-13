e0d3000b84f0df969fca70de81f302462811af0b
Bart van Hoekelen <bvanhoekelen@gmail.com>
2017-03-14 00:34:20 +0100

    Add Performance handler

---
+++ src/Lib/PerformanceHandler.php
@@ -0,0 +1,129 @@
+<?php namespace Performance\Lib;
+
+
+class PerformanceHandler
+{
+    /*
+     * Hold point stack
+     */
+    private $pointStack = [];
+
+    /*
+     *  Hold Point form the first to last measuring point
+     */
+    private $masterPoint;
+
+
+    public function __construct()
+    {
+        // Setup first master point
+        $this->masterPoint = new Point('start');
+    }
+
+    /*
+     * Set measuring point X
+     *
+     * @param string|null   $label
+     * @return void
+     */
+    public function point($label = null)
+    {
+        if( ! $label)
+        {
+            $this->finishAll();
+            $label = 'Task ' . (count($this->pointStack) + 1);
+        }
+
+        // Create and add point to stack
+        $this->addPointToStack(new Point($label));
+    }
+
+    /*
+     * Finish measuring point X
+     *
+     * @param string|null   $label
+     * @return void
+     */
+    public function finish($label = null)
+    {
+        if($label)
+            $this->finishPointByLabel($label);
+        else
+            $this->finishAll();
+    }
+
+    /*
+     * Return test results
+     *
+     * @param array|[]   $config
+     * @return mixed
+     */
+    public function results(array $config = [])
+    {
+        // Finish all
+        $this->finishAll();
+
+        // Close master point
+        $this->masterPoint->finish();
+
+        // Calculate all points
+
+        // Give max time an max memory
+
+        // Print results
+        dd($this);
+
+    }
+
+    // Private
+
+    /*
+     * Add point to stack
+     *
+     * @param Point   $point
+     * @return void
+     */
+    private function addPointToStack(Point $point)
+    {
+        $this->pointStack[] = $point;
+    }
+
+    /*
+     * Finish all point in the stack
+     *
+     * @return void
+     */
+    private function finishAll()
+    {
+        foreach ($this->pointStack as $pointId => $point)
+        {
+            if($point->isActive())
+                $point->finish();
+        }
+
+    }
+
+    /*
+     * Finish point by label in the stack
+     *
+     * @param string    $label (point label)
+     *
+     * @return Boolean
+     */
+    private function finishPointByLabel($label)
+    {
+        $findLabel = false;
+        foreach ($this->pointStack as $pointId => $point)
+        {
+            if($pointId == $label)
+            {
+                $findLabel = true;
+                $point->finish();
+            }
+            break;
+        }
+
+        return $findLabel;
+    }
+
+}
