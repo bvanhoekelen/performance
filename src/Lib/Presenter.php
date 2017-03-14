ac8454ce412301441995bb4e525ccc5ab725ce56
Bart van Hoekelen <bvanhoekelen@gmail.com>
2017-03-14 22:22:01 +0100

    New web style

--- src/Lib/Presenter.php
+++ src/Lib/Presenter.php
@@ -43,31 +43,38 @@ class Presenter {
         include_once 'DisplayHtml.css';
         echo '</style>';
 
-        echo "<div class='performance'>";
-        echo "<h1>Hoi</h1>";
-        echo "<hr>";
-        echo "<table width='100%'>";
-        echo "<tr>";
-            echo "<td width='20%'>Label</td>";
-            echo "<td>Time</td>";
-            echo "<td>Memory usage</td>";
-            echo "<td>Memory peak</td>";
-            echo "<td>Memory max</td>";
-        echo "</tr>";
+        echo '<div class="performance">
+            <table class=\'table-title\'>
+                <tr>
+                    <td width="50%">' . $this->formatter->memoryToHuman( $this->masterPoints->getDifferenceMemory() ) . '</td>
+                    <td width="50%">' . $this->formatter->timeToHuman( $this->masterPoints->getDifferenceTime() )  .  '</td>
+                </tr>
+            </table>
+            
+            <div class="overflow">
+            <table width="100%">
+            <tr>
+                <th width="20%">Label</th>
+                <th width="20%">Time</th>
+                <th width="20%">Memory usage</th>
+                <th width="20%">Memory peak</th>
+                <th width="20%">Memory max</th>
+            </tr>
+            </table>
+            <table width="100%">';
 
-        foreach ($this->points as $point)
-        {
-            echo "<tr>";
-                echo "<td>" . $point->getLabel() . "</td>";
-                echo "<td>" . $this->formatter->timeToHuman( $point->getDifferenceTime() ). "</td>";
-                echo "<td>" . $this->formatter->memoryToHuman( $point->getDifferenceMemory() ). "</td>";
-                echo "<td>" . $this->formatter->memoryToHuman( $point->getMemoryPeak() ). "</td>";
-                echo "<td>" . $this->formatter->memoryToHuman( $point->getStopMemoryUsage() ). "</td>";
-            echo "</tr>";
-
-        }
-
-        echo "</div>";
+            foreach ($this->points as $point)
+            {
+                echo '<tr>';
+                    echo '<td width="20%">' . $point->getLabel() . '</td>';
+                    echo '<td width="20%">' . $this->formatter->timeToHuman( $point->getDifferenceTime() ). '</td>';
+                    echo '<td width="20%">' . $this->formatter->memoryToHuman( $point->getDifferenceMemory() ). '</td>';
+                    echo '<td width="20%">' . $this->formatter->memoryToHuman( $point->getMemoryPeak() ). '</td>';
+                    echo '<td width="20%">' . $this->formatter->memoryToHuman( $point->getStopMemoryUsage() ). '</td>';
+                echo '</tr>';
+            }
+            
+        echo '</div>';
     }
 
     private function displayAsFileOutput()
@@ -80,7 +87,13 @@ class Presenter {
         $data = [];
         foreach ($this->points as $point)
         {
-            $data[] = $point->export();
+
+            $data[] = [
+                'label' => $point->getLabel(),
+                'time' => $this->formatter->timeToHuman( $point->getDifferenceTime() ),
+                'memory_usage' => $this->formatter->memoryToHuman( $point->getDifferenceMemory() ),
+                'memory_peak' => $this->formatter->memoryToHuman( $point->getStopMemoryUsage() ),
+            ];
         }
 
 //        dd(json_encode($data), 1,$this->points);
