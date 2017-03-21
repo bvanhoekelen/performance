9488060c25ed35f7a1b66c59fbd950e76a892ff3
Bart van Hoekelen <bart@stucomm.com>
2017-03-21 19:52:27 +0100

    New test scenario

--- exemple/03_multiple_test.php
+++ exemple/03_multiple_test.php
@@ -6,22 +6,26 @@
 require_once('../vendor/autoload.php');
 use Performance\Performance;
 
+echo "Run";
+
 /*
  * Performing multiple performance test
  */
-Performance::point(1);
+Performance::point('0');
+Performance::point();
+Performance::point();
+Performance::point();
+Performance::point();
+Performance::point();
+Performance::point('A');
+Performance::point('B');
 // Run task A
 $y = [];
 for($x = 0; $x < 200000; $x++)
 {
-    $y[] = $x * 2 * 7;
-    $y[] = $x * 2 * 7;
+    $y[] = $x * 2;
 }
-Performance::point(2);
-Performance::point();
-Performance::point();
-
-Performance::point(); // -> Finish task A and start new point for task B
+Performance::point('C'); // -> Finish task A and start new point for task B
 // Run task B
 for($x = 0; $x < 200000; $x++)
 {
