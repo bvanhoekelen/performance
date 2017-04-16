09e777ccbcf739ef57309e698c43d0f29a07039c
Bart van Hoekelen <bvanhoekelen@gmail.com>
2017-04-16 23:07:39 +0200

    Remove config and changeConfig

--- src/Lib/PerformanceInterface.php
+++ src/Lib/PerformanceInterface.php
@@ -1,11 +1,5 @@
 <?php namespace Performance\Lib;
 
-/**
- * Created by PhpStorm.
- * User: bvanhoekelen
- * Date: 13-03-17
- * Time: 21:35
- */
 interface PerformanceInterface
 {
     /*
@@ -29,22 +23,5 @@ interface PerformanceInterface
      * @return mixed
      */
     public static function results();
-
-    /*
-     * Change config items
-     *
-     * @param
-     * @return mixed
-     */
-    public static function config($configItem);
-
-    /*
-     * Change single config item
-     *
-     * @param string    $item
-     * @param mixed     $value
-     * @return mixed
-     */
-    public static function changeConfig($item, $value);
 
 }
