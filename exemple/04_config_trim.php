c90b3c49317bfbf37e24f88b1a2dc027a8169711
Bart van Hoekelen <bvanhoekelen@gmail.com>
2017-04-17 09:34:22 +0200

    Update exemple

--- exemple/04_config_trim.php
+++ exemple/04_config_trim.php
@@ -8,21 +8,64 @@ require_once('../vendor/autoload.php');
 use Performance\Performance;
 use Performance\Config;
 
-/*
- * Set config item
- */
-Config::set(Config::CONSOLE_LIVE, true);
+// Bootstrap class
+$foo = new Foo();
 
-/*
- * One simply performance check
- */
-Performance::point();
-
-// Run task A
-for($x = 0; $x < 100; $x++)
+class foo
 {
-    echo ".";
-}
+    public function __construct()
+    {
+        // You can specify the characters you want to strip
+        Config::set(Config::POINT_LABEL_LTRIM, 'synchronize');
+        Config::set(Config::POINT_LABEL_RTRIM, 'Run');
 
-// Finish all tasks and show test results
-Performance::results();
+        $this->synchronizeTaskARun();
+        $this->synchronizeTaskBRun();
+        $this->synchronizeTaskCRun();
+
+        // Finish all tasks and show test results
+        Performance::results();
+    }
+
+    public function synchronizeTaskARun()
+    {
+        // Set point Task A
+        Performance::point(__FUNCTION__);
+
+        //
+        // Run code
+        sleep(1);
+        //
+
+        // Finish point Task C
+        Performance::finish();
+    }
+
+    public function synchronizeTaskBRun()
+    {
+        // Set point Task B
+        Performance::point(__FUNCTION__);
+
+        //
+        // Run code
+        sleep(1);
+        //
+
+        // Finish point Task B
+        Performance::finish();
+    }
+
+    public function synchronizeTaskCRun()
+    {
+        // Set point Task C
+        Performance::point(__FUNCTION__);
+
+        //
+        // Run code
+        sleep(1);
+        //
+
+        // Finish point Task C
+        Performance::finish();
+    }
+}
