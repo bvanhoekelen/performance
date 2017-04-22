c6137c85fc0af7ca4c15aa7a4a779d42318fa2a7
Bart van Hoekelen <bvanhoekelen@gmail.com>
2017-04-22 17:10:32 +0200

    Add config query log exemple

---
+++ examples/06_config_query_log.php
@@ -0,0 +1,79 @@
+<?php
+
+/*
+ * Require Performance
+ */
+
+require_once('../vendor/autoload.php');
+require_once('helper/UseEloquent.php');
+
+use Illuminate\Database\Capsule\Manager as DB;
+
+use Performance\Performance;
+use Performance\Config;
+
+// Bootstrap class
+$foo = new Foo();
+
+class Foo
+{
+    public function __construct()
+    {
+        // Enable query log
+        Config::set(Config::QUERY_LOG, true);
+
+        $this->selectQuery();
+        $this->taskB();
+
+
+//        dd(DB::getQueryLog());
+
+        // Finish all tasks and show test results
+        Performance::results();
+    }
+
+    public function selectQuery()
+    {
+        // Set point Task A
+        Performance::point(__FUNCTION__);
+
+
+
+        $users = DB::table('user')->select('*')
+            ->where('id', '<=', '1077')
+            ->get();
+
+        $user = DB::table('user')->select('*')
+            ->where('name', 'Bart')
+            ->first();
+
+        $user->email = 'bart@gmail.com';
+
+        $user->save();
+
+//        $users = DB::table('user')->select('*')
+//            ->where('id', '<=', '1072')
+//            ->delete();
+
+        foreach($users as $user)
+        {
+            echo $user->name . PHP_EOL;
+        }
+
+        // Finish point Task A
+        Performance::finish();
+    }
+
+    public function taskB()
+    {
+        // Set point Task B
+        Performance::point(__FUNCTION__);
+
+        //
+        // Run code
+        //
+
+        // Finish point Task B
+        Performance::finish();
+    }
+}
---
+++ examples/helper/UseEloquent.php
@@ -0,0 +1,25 @@
+<?php
+
+////Use laravel Eloquent for database connection
+use Illuminate\Database\Capsule\Manager as DB;
+
+$capsule = new DB();
+$capsule->addConnection([
+    'driver' => 'mysql',
+    'host' => '127.0.0.1',
+    'port' => '8889',
+    'database' => 'performance',
+    'username' => 'root',
+    'password' => 'root',
+]);
+
+// Set the event dispatcher used by Eloquent models... (optional)
+use Illuminate\Events\Dispatcher;
+use Illuminate\Container\Container;
+$capsule->setEventDispatcher(new Dispatcher(new Container));
+
+// Make this Capsule instance available globally via static methods... (optional)
+$capsule->setAsGlobal();
+
+// Bootstrap eloquet
+$capsule->bootEloquent();
