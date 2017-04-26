e84fa12d2865b8239ac3c9e0b8f3474c61f113c1
Bart van Hoekelen <bvanhoekelen@gmail.com>
2017-04-26 16:19:57 +0200

    Add test

---
+++ tests/Unit/T07A_ConfigQueryLogTest.php
@@ -0,0 +1,49 @@
+<?php namespace Tests\Unit;
+
+use Performance\Performance;
+use Performance\Config;
+
+
+class T07A_ConfigQueryLogTest extends \PHPUnit_Framework_TestCase
+{
+    public function testConfigQueryLog()
+    {
+        // Reset
+        Performance::instanceReset();
+
+        // You can specify the characters you want to strip
+        Config::setQueryLog(false);
+        Config::setQueryLog(true);
+        Config::setQueryLog(true, 'resume');
+        Config::setQueryLog(true, 'full');
+        Config::setQueryLog(false, 'full');
+
+        // Run task A
+        $this->taskA();
+
+        // Finish all tasks and show test results
+        Performance::results();
+    }
+
+    public function testConfigQueryLogIsTrue()
+    {
+        $configItem = Performance::instance()->config->isQueryLog();
+        $this->assertFalse($configItem);
+    }
+
+    // Create task
+
+    private function taskA()
+    {
+        // Set point Task A
+        Performance::point(__FUNCTION__);
+
+        //
+        // Run code
+        usleep(2000);
+        //
+
+        // Finish point Task A
+        Performance::finish();
+    }
+}
