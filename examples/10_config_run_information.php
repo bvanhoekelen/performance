389b2e3b392d6306e174974db87a7a32552339fe
Bart van Hoekelen <bvanhoekelen@gmail.com>
2017-06-28 13:16:09 +0200

    Add test and exemple

---
+++ examples/10_config_run_information.php
@@ -0,0 +1,30 @@
+<?php
+
+/*
+ * Require Performance
+ */
+
+require_once('../vendor/autoload.php');
+use Performance\Performance;
+use Performance\Config;
+
+/*
+ * Set config item
+ */
+Config::setConsoleLive(true);
+Config::setRunInformation(true);
+
+/*
+ * One simply performance check
+ */
+Performance::point();
+
+// Run task A
+for($x = 0; $x < 100; $x++)
+{
+    echo ".";
+    usleep(3000);
+}
+
+// Finish all tasks and show test results
+Performance::results();
---
+++ tests/Unit/T10B_ConfigRunInformationTest.php
@@ -0,0 +1,13 @@
+<?php namespace Tests\Unit;
+
+use Performance\Config;
+use Performance\Performance;
+
+class T10B_ConfigRunInformationTest extends T10A_ConfigRunInformationTest
+{
+    protected function setTestUp()
+    {
+        Performance::instanceReset();
+        Config::setPresenter('web');
+    }
+}
