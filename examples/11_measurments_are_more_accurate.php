b93349aa7c224a2c4780b7ed91b95599a0058594
Bart van Hoekelen <bvanhoekelen@gmail.com>
2017-09-13 20:46:03 +0200

    Add example for testing measurement more accurate

---
+++ examples/11_measurments_are_more_accurate.php
@@ -0,0 +1,27 @@
+<?php
+
+/*
+ * Require Performance
+ */
+
+require_once('../vendor/autoload.php');
+use Performance\Performance;
+
+/*
+ * One test for measurements the accurate
+ */
+Performance::point('1A');
+Performance::point('1B');
+Performance::point('1C');
+Performance::point('1D');
+
+Performance::point('2A');
+Performance::finish();
+Performance::point('2B');
+Performance::finish();
+Performance::point('2C');
+Performance::finish();
+Performance::point('2D');
+
+// Finish all tasks and show test results
+Performance::results();
