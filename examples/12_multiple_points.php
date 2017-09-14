a32179e119682a75c247749483bf71f0d5ffc695
Bart van Hoekelen <bvanhoekelen@gmail.com>
2017-09-14 13:41:14 +0200

    Update exemple

--- examples/12_multiple_points.php
+++ examples/12_multiple_points.php
@@ -8,10 +8,11 @@ require_once('../vendor/autoload.php');
 use Performance\Performance;
 
 /*
- * One simply performance check
+ * Multiple point example
+ * Note: Multiple point are less accurate!
  */
 
-//
+// Start multiple point 1
 Performance::point('Multiple point 1', true);
 
 Performance::point('Normal point 1');
@@ -19,16 +20,14 @@ Performance::finish();
 Performance::point('Normal point 2');
 Performance::finish();
 Performance::point('Normal point 3');
+
+// Start multiple point 2
 Performance::point('Multiple point 2', true);
 Performance::point('Normal point 4');
 
+// Stop multiple points 1 and normal point 4
 Performance::finish('Multiple point 1');
-Performance::finish('Multiple point 2');
-
 
 // Finish all tasks and show test results
+// Multiple point 2 wil bee automatic finish
 Performance::results();
-
-$export = Performance::export();
-
-//dd($export);
