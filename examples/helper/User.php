6911e98cddb9caf99364d486ae53d35d6e4c37c9
Bart van Hoekelen <bvanhoekelen@gmail.com>
2018-10-05 22:37:14 +0200

    Add table info

--- examples/helper/User.php
+++ examples/helper/User.php
@@ -5,5 +5,13 @@ class User extends Illuminate\Database\Eloquent\Model
     protected $table = 'user';
 
     public $timestamps = false;
-
 }
+
+// Table info
+//
+// CREATE TABLE `user` (
+//  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+//  `name` varchar(255) DEFAULT NULL,
+//  `email` varchar(255) DEFAULT NULL,
+//  PRIMARY KEY (`id`)
+//) ENGINE=InnoDB DEFAULT CHARSET=utf8;
